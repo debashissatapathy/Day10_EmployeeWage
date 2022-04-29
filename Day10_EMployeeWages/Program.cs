35f032a1aeb668dc6188803e34682d98d0132414
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-29 12:51:31 +0530

    [Debashis] Added---message

---
+++ Day10_EMployeeWages/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day10_EMployeeWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWage Computation Program");
+            UC1_Attendance uc1 = new UC1_Attendance();
+            uc1.Attendance();
+        }
+    }
+}
---
+++ Day10_EMployeeWages/UC1_Attendance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day10_EMployeeWages
+{
+    class UC1_Attendance
+    {
+        public void Attendance()
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int chk_emp = random.Next(0, 2);
+            if (chk_emp == isPresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+                Console.WriteLine("Employee is absent");
+        }
+    }
+}
